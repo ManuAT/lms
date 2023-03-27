a03102de65868ce76c823da581f4d523f5f4afa3
Manu A T <38802051+ManuAT@users.noreply.github.com>
2023-03-27 16:12:32 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.cluster0-axhr4.mongodb.net:27017/uwO3mypu',
+    remoteUrl : process.env.remoteUrl,
     localUrl: 'mongodb://localhost/manu'
 };
